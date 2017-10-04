e3de3e9dbdc31c17e6d2cf65c39c293ab20c4d74
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-04 12:40:57 +0300

    Add navigation bar

---
+++ src/components/Common/NavBar/NavBar.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Button from 'material-ui/Button';
+import IconButton from 'material-ui/IconButton';
+import MenuIcon from 'material-ui-icons/Menu';
+import './style.less';
+
+export interface Props {}
+
+class NavBar extends React.Component<Props, object> {
+  render() {
+    return (
+      <div className="navbar">
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography type="title" color="inherit" className="title">
+              CarFinder
+            </Typography>
+            <div className="items-left">
+              <Button color="contrast">
+                <Link to="/">Home</Link>
+              </Button>
+              <Button color="contrast">
+                <Link to="/catalog">Catalog</Link>
+              </Button>
+            </div>
+            <div className="items-right">
+              <Button color="contrast">
+                <Link to="/signup">Sign in</Link>
+              </Button>
+            </div>
+          </Toolbar>
+        </AppBar>
+      </div>
+    );
+  }
+}
+
+export default NavBar;
--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,10 +1,21 @@
 import * as React from 'react';
+import NavBar from '../Common/NavBar/NavBar';
+import Grid from 'material-ui/Grid';
 
 export interface Props {}
 
 class HomePage extends React.Component<Props, object> {
   render() {
-    return <div>Hi from HomePage</div>;
+    return (
+      <div>
+        <NavBar />
+        <Grid container>
+          <Grid item xs={12}>
+            <p>Home Page</p>
+          </Grid>
+        </Grid>
+      </div>
+    );
   }
 }
 
