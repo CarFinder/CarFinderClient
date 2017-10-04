8cb0ce8779c3c01e47e9bbdeb06428b2cc288a17
Unknown <lizaburaya@gmail.com>
2017-10-05 00:37:19 +0300

    Implement SigninForm component

---
+++ src/components/SigninForm/SigninForm.tsx
@@ -0,0 +1,66 @@
+import * as React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { Link } from 'react-router-dom';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import Divider from 'material-ui/Divider';
+import Paper from 'material-ui/Paper';
+import { required, email } from '../../utils/validation';
+import './style.less';
+
+export interface Props {
+  input?: any;
+  name?: any;
+  label?: any;
+  meta?: any;
+  handleSubmit: any;
+}
+
+const renderTextField = ({ input, label, meta: { touched, error }, ...custom }: Props) => (
+  <div>
+    <TextField className="form-input" errorText={touched && error} {...input} {...custom} />
+    {touched && (error && <span className="error">{error}</span>)}
+  </div>
+);
+
+const SigninForm = (props: Props) => {
+  return (
+    <div className="signin-form">
+      <Paper className="form-container">
+        <form className="form-content" onSubmit={props.handleSubmit}>
+          <h1>Вход</h1>
+          <div>
+            <Field
+              name="email"
+              component={renderTextField}
+              placeholder="Email"
+              validate={[required, email]}
+            />
+          </div>
+          <div>
+            <Field
+              name="password"
+              component={renderTextField}
+              placeholder="Пароль"
+              validate={required}
+            />
+          </div>
+          <div>
+            <Button className="form-button" type="submit" raised>
+              Войти
+            </Button>
+          </div>
+          <Divider />
+          <div className="form-links">
+            <Link to="/restore-password">Забыли пароль?</Link>
+            <Link to="/signup">Зарегистрироваться</Link>
+          </div>
+        </form>
+      </Paper>
+    </div>
+  );
+};
+
+export default reduxForm({
+  form: 'SigninForm'
+})(SigninForm);
