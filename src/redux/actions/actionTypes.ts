07b0364ddda7fd4968f6f945a88439ddcf16afe5
Elizaveta <lizaburaya@gmail.com>
2017-10-05 21:09:50 +0300

    Create GET_USERDATA type

--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -3,3 +3,6 @@ export type USER_LOGGED_IN = typeof USER_LOGGED_IN;
 
 export const USER_LOGGED_OUT = 'USER_LOGGED_OUT ';
 export type USER_LOGGED_OUT = typeof USER_LOGGED_OUT;
+
+export const GET_USERDATA = 'GET_USERDATA';
+export type GET_USERDATA = typeof GET_USERDATA;
