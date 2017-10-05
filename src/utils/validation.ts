4836527d141adc67e1c20193b9f8edfebd16632e
Elizaveta <lizaburaya@gmail.com>
2017-10-05 21:04:24 +0300

    Create password validation rule

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -4,3 +4,8 @@ export const email = (value: string) =>
   value && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value)
     ? 'Неверный формат email: name@example.com'
     : undefined;
+
+export const password = (value: string) =>
+  value && !/^(?=.*[A-Za-z])(?=.*\d)(?=.*[$@$!%*#?&])[A-Za-z\d$@$!%*#?&]{8,}$/i.test(value)
+    ? 'Неверный формат пароля: не менее 8 символов, по крайней мере одца цифра и один специальный символ'
+    : undefined;
