3c6b597886472cb281746821804fa798c9495707
Elizaveta <lizaburaya@gmail.com>
2017-10-09 17:58:00 +0300

    Nest signin form into grid component

--- src/components/SigninForm/SigninForm.tsx
+++ src/components/SigninForm/SigninForm.tsx
@@ -1,90 +1,65 @@
 import * as React from 'react';
-import { Field, reduxForm } from 'redux-form';
-import { Link, Redirect } from 'react-router-dom';
-import TextField from 'material-ui/TextField';
-import Button from 'material-ui/Button';
+import { Link } from 'react-router-dom';
+import Grid from 'material-ui/Grid';
 import Divider from 'material-ui/Divider';
+import Button from 'material-ui/Button';
 import Paper from 'material-ui/Paper';
-import CircularProgress from 'material-ui/Progress/CircularProgress';
-import { validateSignin as validate } from '../../utils/signinValidation';
+import Typography from 'material-ui/Typography';
+import { CircularProgress } from 'material-ui/Progress';
+import Form from './WizardForm/SigninForm';
+import { UserData } from '../../containers/Signin';
+
 import './style.less';
 
 export interface Props {
-  input?: HTMLInputElement;
-  name?: string;
-  title?: string;
-  meta?: any;
-  custom?: object;
-  handleSubmit: any;
-  handleLogIn?: () => void;
-  history?: any;
-  invalid?: boolean;
-  loading?: boolean;
-  authError?: string;
+  handleLogIn: (userData: UserData) => any;
+  loading: boolean;
+  authError?: any;
 }
 
-const renderTextField = ({ title, input, meta: { touched, error }, ...custom }: Props) => (
-  <div>
-    <TextField
-      name={name}
-      label={title}
-      className="form-input"
-      helperText={touched && error}
-      error={touched && !!error}
-      {...input}
-      {...custom}
-    />
-  </div>
-);
+export interface State {
+  page: number;
+}
 
-const SigninForm = (props: Props) => {
-  if (localStorage.getItem('jwt')) {
-    props.history.push('/home');
+const SigninFrom = (props: Props) => {
+  const { loading, authError } = props;
+
+  function handleSubmit(userData: UserData) {
+    props.handleLogIn(userData);
   }
   return (
-    <div className="signin-form">
-      <Paper className="form-container">
-        <form className="form-content" onSubmit={props.handleSubmit(props.handleLogIn)}>
-          <h1>Вход</h1>
-          <div>
-            <Field
-              name="email"
-              type="email"
-              component={renderTextField}
-              placeholder="Email"
-              title="Email"
-            />
-          </div>
-          <div>
-            <Field
-              name="password"
-              type="password"
-              component={renderTextField}
-              placeholder="Пароль"
-              title="Пароль"
-            />
-          </div>
-          {props.authError && <span className="error">{props.authError}</span>}
-          <div>
-            {props.loading && <CircularProgress />}
-            {!props.loading && (
-              <Button className="form-button" type="submit" disabled={props.invalid} raised>
-                Войти
-              </Button>
-            )}
-          </div>
-          <Divider />
-          <div className="form-links">
-            <Link to="/restore-password">Забыли пароль?</Link>
-            <Link to="/signup">Зарегистрироваться</Link>
-          </div>
-        </form>
-      </Paper>
+    <div className="signup-form">
+      <Grid container>
+        <Grid item xs={12}>
+          <Grid container align="center" direction="column" justify="center">
+            <Paper className="form-container">
+              <Grid item className="form-title">
+                <Typography type="display1">Вход</Typography>
+              </Grid>
+              <Grid item className="form-content">
+                {authError && (
+                  <Typography type="body1" component="p" color="accent">
+                    {authError.ruMessage}
+                  </Typography>
+                )}
+                <Form onSubmit={handleSubmit} />
+                {loading && <CircularProgress size={50} />}
+              </Grid>
+              <Divider />
+              <Grid item className="form-links">
+                <Button dense color="accent">
+                  <Link to="/restore-password">Забыли пароль?</Link>
+                </Button>
+                <Button dense color="accent">
+                  <Link to="/signup">Зарегистрироваться</Link>
+                </Button>
+              </Grid>
+            </Paper>
+          </Grid>
+        </Grid>
+      </Grid>
     </div>
   );
 };
 
-export default reduxForm({
-  form: 'SigninForm',
-  validate
-})(SigninForm);
+export default SigninFrom;
