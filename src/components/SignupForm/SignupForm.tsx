22a31bf74ad85700b24059c46ebd9e459da467b0
Anton Stankovsky <stankovsky.ant@gmail.com>
2017-10-09 17:50:02 +0300

    Revert "Signup"

--- src/components/SignupForm/SignupForm.tsx
+++ src/components/SignupForm/SignupForm.tsx
@@ -1,102 +1,10 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
-import Grid from 'material-ui/Grid';
-import Divider from 'material-ui/Divider';
-import Button from 'material-ui/Button';
-import Paper from 'material-ui/Paper';
-import Typography from 'material-ui/Typography';
-import { CircularProgress } from 'material-ui/Progress';
-import FormStepper from './WizardForm/FormStepper';
-import FirstPage from './WizardForm/FirstPage';
-import SecondPage from './WizardForm/SecondPage';
-import ThirdPage from './WizardForm/ThirdPage';
-import LastPage from './WizardForm/LastPage';
-import { UserData } from '../../containers/Signup';
 
-import './style.less';
+export interface Props {}
 
-export interface Props {
-  handleSignup: (userData: UserData) => any;
-  loading: boolean;
-  signedup: boolean;
-  authError?: any;
-}
-
-export interface State {
-  page: number;
-}
-
-class SignupFrom extends React.Component<Props, State> {
-  constructor() {
-    super();
-    this.state = {
-      page: 1
-    };
-  }
-
-  componentWillReceiveProps(props: Props) {
-    if (props.signedup) {
-      this.setState({
-        page: 4
-      });
-    } else if (props.authError) {
-      this.setState({
-        page: 1
-      });
-    }
-  }
-
-  handleSubmit = (userData: UserData) => {
-    this.props.handleSignup(userData);
-  };
-
-  nextPage = () => {
-    this.setState({ page: this.state.page + 1 });
-  };
-
+class SignupFrom extends React.Component<Props, object> {
   render() {
-    const { page } = this.state;
-    const { loading, authError } = this.props;
-    return (
-      <div className="signup-form">
-        <Grid container>
-          <Grid item xs={12}>
-            <Grid container align="center" direction="column" justify="center">
-              <Paper className="form-container">
-                {page !== 4 && (
-                  <Grid item className="form-title">
-                    <Typography type="display1">Регистрация</Typography>
-                  </Grid>
-                )}
-                {page !== 4 && (
-                  <Grid item className="form-stepper">
-                    <FormStepper page={page} />
-                  </Grid>
-                )}
-                <Grid item className="form-content">
-                  {authError && (
-                    <Typography type="subheading" component="p" color="accent">
-                      {authError}
-                    </Typography>
-                  )}
-                  {page === 1 && <FirstPage onSubmit={this.nextPage} />}
-                  {page === 2 && <SecondPage onSubmit={this.nextPage} />}
-                  {page === 3 && <ThirdPage onSubmit={this.handleSubmit} />}
-                  {page === 4 && <LastPage />}
-                  {loading && <CircularProgress size={50} />}
-                </Grid>
-                <Divider />
-                <Grid item className="form-links">
-                  <Button dense color="accent">
-                    <Link to="/signin">Уже зарегестрированы? Войти.</Link>
-                  </Button>
-                </Grid>
-              </Paper>
-            </Grid>
-          </Grid>
-        </Grid>
-      </div>
-    );
+    return <div>Hi from Signup Form</div>;
   }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,59 +1,6 @@
 // React
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-// Redux
-import { Provider } from 'react-redux';
-import { Store, createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import createSagaMiddleware from 'redux-saga';
-import rootReducer from './redux/reducers';
-import rootSaga from './redux/sagas/index';
-// Styles
-import 'index.less';
-// Components
-import NavBar from './components/Common/NavBar/NavBar';
-import Signup from './containers/Signup';
-import Home from './components/HomePage/HomePage';
-import EmailConfirmation from './containers/EmailConfirmation';
-import NotFound from './components/Common/NotFound/NotFound';
-
-const App = () => {
-  return (
-    <div>
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/signup" component={Signup} />
-        <Route path="/confirmation/:token" component={EmailConfirmation} />
-        <Route path="*" component={NotFound} />
-      </Switch>
-    </div>
-  );
-};
-
-const sagaMiddleware = createSagaMiddleware();
-const initialState = {};
-const composeEnhansers = composeWithDevTools || compose;
-const store: Store<any> = createStore(
-  rootReducer,
-  initialState,
-  composeEnhansers(applyMiddleware(sagaMiddleware))
-);
-sagaMiddleware.run(rootSaga);
-
-ReactDOM.render(
-  <BrowserRouter>
-    <Provider store={store}>
-      <Route component={App} />
-    </Provider>
-  </BrowserRouter>,
-  document.getElementById('app')
-);
-=======
-// React
-import React from 'react';
-import ReactDOM from 'react-dom';
 import { BrowserRouter, Route } from 'react-router-dom';
 // Redux
 import { Provider } from 'react-redux';
