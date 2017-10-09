bea01cb227e78e6aae52036b89cec32a975e4223
Elizaveta <lizaburaya@gmail.com>
2017-10-09 15:16:19 +0300

    Add action types

--- src/redux/actions/actionTypes.ts
+++ src/redux/actions/actionTypes.ts
@@ -6,3 +6,18 @@ export type USER_LOGGED_OUT = typeof USER_LOGGED_OUT;
 
 export const USER_SIGN_IN = 'USER_SIGN_IN';
 export type USER_SIGN_IN = typeof USER_SIGN_IN;
+
+export const USER_CONFIRM_EMAIL = 'USER_CONFIRM_EMAIL';
+export type USER_CONFIRM_EMAIL = typeof USER_CONFIRM_EMAIL;
+
+export const USER_CONFIRM_EMAIL_SUCCESS = 'USER_CONFIRM_EMAIL_SUCCESS';
+export type USER_CONFIRM_EMAIL_SUCCESS = typeof USER_CONFIRM_EMAIL_SUCCESS;
+
+export const SET_LOADING = 'SET_LOADING';
+export type SET_LOADING = typeof SET_LOADING;
+
+export const SET_AUTH_ERROR = 'SET_AUTH_ERROR';
+export type SET_AUTH_ERROR = typeof SET_AUTH_ERROR;
+
+export const SET_SEARCH_ERROR = 'SET_SEARCH_ERROR';
+export type SET_SEARCH_ERROR = typeof SET_SEARCH_ERROR;
