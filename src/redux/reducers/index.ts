451833f08d76d2397f4946e006c2dc023836e1a6
Elizaveta <lizaburaya@gmail.com>
2017-10-09 15:15:18 +0300

    Combine reducers

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import userReducer from './user';
+import formStateReducer from './formState';
 import { reducer as reduxFormReducer } from 'redux-form';
 
+
 const rootReducer = combineReducers({
-  user: userReducer, form: reduxFormReducer
+  user: userReducer, form: reduxFormReducer, formState: formStateReducer
 });
 
 export default rootReducer;
