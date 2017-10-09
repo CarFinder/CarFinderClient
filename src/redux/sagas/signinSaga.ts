6689d1db22059882b19eb6e62097b299d8c03bfd
Elizaveta <lizaburaya@gmail.com>
2017-10-09 17:49:20 +0300

    Fix naming, data types and payloads

--- src/redux/sagas/signinSaga.ts
+++ src/redux/sagas/signinSaga.ts
@@ -4,25 +4,25 @@ import { call, put, takeEvery, takeLatest } from 'redux-saga/effects'
 import { SagaIterator } from 'redux-saga';
 import jwt_decode from 'jwt-decode';
 
-function* fetchUser(userData: any) {
+function* callSignin(action: any) {
     yield put({type: "SET_LOADING", payload: true});
     try {
-        const { response, error } = yield call(API.user.getUser, userData.payload)
+        const { response, error } = yield call(API.user.getUser, action.payload)
         if (response) {
             const decodedData = jwt_decode(response.token);
             localStorage.setItem('jwt', response.token);
             yield put({type: "SET_LOADING", payload: false});
-            yield put({type: "USER_LOGGED_IN", user: decodedData});
+            yield put({type: "USER_SIGN_IN_SUCCESS", payload: decodedData});
         } else {
             yield put({type: "SET_LOADING", payload: false});
-            yield put({type: "SET_AUTH_ERROR", payload: "Cannot reach server"});
+            yield put({type: "SET_AUTH_ERROR", payload: error});
         }
     } catch (e) {
         yield put({type: "SET_LOADING", payload: false});
-        yield put({type: "SET_AUTH_ERROR", payload: e.message});
+        yield put({type: "SET_AUTH_ERROR", payload: e.messadge});
     }
  }
  
 export default function* watchLogIn(): SagaIterator {
-    yield takeEvery("USER_SIGN_IN", fetchUser);
+    yield takeEvery("USER_SIGN_IN", callSignin);
 }
