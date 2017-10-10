4c36cc204319c9d4d820ddcb32442de2c5607b69
Elizaveta <lizaburaya@gmail.com>
2017-10-10 15:50:36 +0300

    Fix: change response object name

--- src/redux/sagas/signinSaga.ts
+++ src/redux/sagas/signinSaga.ts
@@ -8,14 +8,14 @@ function* callSignin(action: any) {
   yield put({ type: 'SET_LOADING', payload: true });
   try {
     const response = yield call(API.user.getUser, action.payload);
-    if (!response.error) {
-      const decodedData = jwt_decode(response.token);
-      localStorage.setItem('jwt', response.token);
+    if (!response.data.error) {
+      const decodedData = jwt_decode(response.data.token);
+      localStorage.setItem('jwt', response.data.token);
       yield put({ type: 'SET_LOADING', payload: false });
       yield put({ type: 'USER_SIGN_IN_SUCCESS', payload: decodedData });
     } else {
       yield put({ type: 'SET_LOADING', payload: false });
-      yield put({ type: 'SET_AUTH_ERROR', payload: response.error });
+      yield put({ type: 'SET_AUTH_ERROR', payload: response.data.error });
     }
   } catch (e) {
     yield put({ type: 'SET_LOADING', payload: false });
