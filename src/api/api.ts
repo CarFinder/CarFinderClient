53ebe416e7acb08f4af270feed2847d52a444358
Elizaveta <lizaburaya@gmail.com>
2017-10-13 16:32:44 +0300

    Add api for restore password

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 import { UserData } from '../containers/Signin';
+import { UserData as RestorePasswordUserData } from '../containers/ChangePassword';
 
 export default {
   user: {
     getUser: (data: UserData) => axios.post('/api/user/signin', data),
     signup: (data: UserData) => axios.post('/api/user/register', data),
-    confirmEmail: (token: string) => axios.post('/api/user/confirm', { token })
+    confirmEmail: (token: string) => axios.post('/api/user/confirm', { token }),
+    submitEmail: (data: RestorePasswordUserData) => axios.post('/api/user/forgot', data),
+    changePassword: (data: RestorePasswordUserData) => axios.post('/api/user/restore', { data })
   }
 };
