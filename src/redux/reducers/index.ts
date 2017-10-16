4eb2b9d49d274df415beb1f11ddb988e80f02771
Elizaveta <lizaburaya@gmail.com>
2017-10-16 20:09:17 +0300

    Create filterResults reducer

--- src/redux/reducers/index.ts
+++ src/redux/reducers/index.ts
@@ -1,12 +1,14 @@
 import { Action, combineReducers, Reducer } from 'redux';
 import { reducer as reduxFormReducer } from 'redux-form';
+import filterResultsReducer from './filterResults';
 import formStateReducer from './formState';
 import userReducer from './user';
 
 const rootReducer: Reducer<any> = combineReducers({
   form: reduxFormReducer,
   formState: formStateReducer,
-  user: userReducer
+  user: userReducer,
+  filterResults: filterResultsReducer
 });
 
 export default rootReducer;
