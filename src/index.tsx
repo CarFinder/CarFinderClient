a3ab1cabd2ff59c0de07a932b7b612c83ca85c39
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-16 15:56:53 +0300

    Define route for catalog

--- src/index.tsx
+++ src/index.tsx
@@ -10,10 +10,11 @@ import NavBar from './components/Common/NavBar/NavBar';
 import NotFound from './components/Common/NotFound/NotFound';
 import RequireAuth from './components/Common/Routes/RequireAuth';
 import Home from './components/HomePage/HomePage';
+import Catalogue from './containers/Catalogue';
+import ChangePassword from './containers/ChangePassword';
 import EmailConfirmation from './containers/EmailConfirmation';
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
-import ChangePassword from './containers/ChangePassword';
 import rootReducer from './redux/reducers';
 import rootSaga from './redux/sagas/index';
 
@@ -24,6 +25,7 @@ const App = () => {
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={RequireAuth(Home)} />
+        <Route path="/catalog" component={RequireAuth(Catalogue)} />
         <Route path="/signup" component={Signup} />
         <Route path="/signin" component={Signin} />
         <Route path="/restore" component={ChangePassword} />
