2af89b2dd1fafcee7fdd6182ac988607ed3ea6c6
Elizaveta <lizaburaya@gmail.com>
2017-10-16 17:10:39 +0300

    Feature: auto login

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import 'index.less';
+import jwt_decode from 'jwt-decode';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
@@ -10,20 +11,30 @@ import NavBar from './components/Common/NavBar/NavBar';
 import NotFound from './components/Common/NotFound/NotFound';
 import RequireAuth from './components/Common/Routes/RequireAuth';
 import Home from './components/HomePage/HomePage';
+import Catalogue from './containers/Catalogue';
+import ChangePassword from './containers/ChangePassword';
 import EmailConfirmation from './containers/EmailConfirmation';
 import Signin from './containers/Signin';
 import Signup from './containers/Signup';
-import ChangePassword from './containers/ChangePassword';
+import { User } from './redux//models/userModel';
+import * as actions from './redux/actions/userActions';
 import rootReducer from './redux/reducers';
 import rootSaga from './redux/sagas/index';
+import setAuthorizationHeader from './utils/axiosHeader';
 
 const App = () => {
+  if (localStorage.jwt) {
+    const payload = jwt_decode(localStorage.jwt);
+    setAuthorizationHeader(localStorage.jwt);
+    store.dispatch(actions.userLoggedIn(payload));
+  }
   return (
     <div>
       <NavBar />
       <Switch>
         <Route exact path="/" component={Home} />
         <Route exact path="/home" component={RequireAuth(Home)} />
+        <Route exact path="/catalog" component={Catalogue} /> //add RequireAuth
         <Route path="/signup" component={Signup} />
         <Route path="/signin" component={Signin} />
         <Route path="/restore" component={ChangePassword} />
