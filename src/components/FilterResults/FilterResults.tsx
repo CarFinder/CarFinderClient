4641b9428514a0457330b363724d98b0772d42af
Elizaveta <lizaburaya@gmail.com>
2017-10-16 20:05:15 +0300

    Add logic to FilterResults component

--- src/components/FilterResults/FilterResults.tsx
+++ src/components/FilterResults/FilterResults.tsx
@@ -1,15 +1,68 @@
+import Grid from 'material-ui/Grid';
+import { CircularProgress } from 'material-ui/Progress';
 import * as React from 'react';
-import interfaceLanguage from '../../utils/interfaceLanguage';
+import InfiniteScroll from 'react-infinite-scroller';
+import { CarModel } from '../../redux/models/filterResultsModel';
+import CarAd from './CarAd/CarAd';
 import './style.less';
 
 export interface Props {
-  language: string;
+  filterResults: CarModel[];
 }
 
-class CarFilter extends React.Component<Props, any> {
+export interface State {
+  index: number;
+}
+
+class FilterResults extends React.Component<Props, State> {
+  constructor() {
+    super();
+    this.state = {
+      index: 20
+    };
+  }
+
+  public loadFunc = () => {
+    if (this.state.index) {
+      this.setState({
+        index: this.state.index + 20
+      });
+    }
+  };
+
   public render() {
-    return <div />;
+    const { filterResults } = this.props;
+    const items: any[] = [];
+    filterResults.forEach((value: CarModel, index: number) => {
+      if (index < this.state.index) {
+        items.push(
+          <Grid item className="ad" key={index}>
+            <CarAd
+              model={value.model}
+              mark={value.mark}
+              description={value.description}
+              price={value.price}
+              year={value.year}
+              images={value.images}
+              mileFrom={value.mileFrom}
+            />
+          </Grid>
+        );
+      }
+    });
+    return (
+      <Grid container className="ads-container">
+        <InfiniteScroll
+          className="infinite-scroll-container"
+          loadMore={this.loadFunc}
+          hasMore={true}
+          loader={<CircularProgress size={50} />}
+        >
+          {items}
+        </InfiniteScroll>
+      </Grid>
+    );
   }
 }
 
-export default CarFilter;
+export default FilterResults;
