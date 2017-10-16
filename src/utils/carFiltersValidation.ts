a8477a6edc8cd9c9f968fa2fbd69811dc8f258a9
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-16 18:12:36 +0300

    Validation function for maker field

---
+++ src/utils/carFiltersValidation.ts
@@ -0,0 +1,23 @@
+interface FormData {
+  mark: string;
+  model: string;
+  bodyType: string;
+  yearFrom: number;
+  yearTo: number;
+  priceForm: number;
+  priceTo: number;
+  kmsFrom: number;
+  kmsTo: number;
+}
+
+export const validateMark = (mark: string): any => {
+  const errors: any = {};
+  if (!mark) {
+    errors.mark = 'Field is required';
+  }
+};
+
+export const validateForm = (values: FormData): any => {
+  const errors: any = {};
+  return errors;
+};
