895de49e1e296ce5ef424d16ca9bbf777ee0435a
Elizaveta <lizaburaya@gmail.com>
2017-10-16 14:31:53 +0300

    Create FilterResults initial component

---
+++ src/components/FilterResults/FilterResults.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import interfaceLanguage from '../../utils/interfaceLanguage';
+import './style.less';
+
+export interface Props {
+  language: string;
+}
+
+class CarFilter extends React.Component<Props, any> {
+  public render() {
+    return <div />;
+  }
+}
+
+export default CarFilter;
