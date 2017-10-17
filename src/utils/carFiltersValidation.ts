2c6dfdfbdb537a2a2aa6acfb1c2c68fc84a8b25c
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-17 12:44:46 +0300

    Validation for car filters

--- src/utils/carFiltersValidation.ts
+++ src/utils/carFiltersValidation.ts
@@ -10,14 +10,48 @@ interface FormData {
   kmsTo: number;
 }
 
+const yearValidation = new RegExp('^(194[5-9]|19[5-9]d|200d|201[0-3])$');
+
 export const validateMark = (mark: string): any => {
   const errors: any = {};
   if (!mark) {
     errors.mark = 'Field is required';
   }
+  return errors;
+};
+
+export const validateYears = (year: number): string => {
+  if (!yearValidation.test(String(year))) {
+    return 'Please fill in a valid year';
+  } else {
+    return '';
+  }
+};
+
+export const validatePairValues = (from: number, to: number): string => {
+  if (from > to) {
+    return 'Invalid parameters';
+  } else {
+    return '';
+  }
 };
 
 export const validateForm = (values: FormData): any => {
   const errors: any = {};
+  if (values.yearFrom) {
+    errors.yearFrom = validateYears(values.yearFrom);
+  }
+  if (values.yearTo) {
+    errors.yearTo = validateYears(values.yearTo);
+  }
+  if (values.yearFrom && values.yearTo) {
+    errors.yearTo = validatePairValues(values.yearFrom, values.yearTo);
+  }
+  if (values.priceFrom && values.priceTo) {
+    errors.priceTo = validatePairValues(values.priceFrom, values.priceTo);
+  }
+  if (values.kmsFrom && values.kmsTo) {
+    errors.kmsTo = validatePairValues(values.kmsFrom, values.kmsTo);
+  }
   return errors;
 };
