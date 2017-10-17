7da7bff6142b55ef40055c065161bd3cc12a4ee6
Elizaveta <lizaburaya@gmail.com>
2017-10-17 19:00:57 +0300

    Change FilterResults component logic

--- src/components/FilterResults/FilterResults.tsx
+++ src/components/FilterResults/FilterResults.tsx
@@ -2,6 +2,8 @@ import Grid from 'material-ui/Grid';
 import { CircularProgress } from 'material-ui/Progress';
 import * as React from 'react';
 import InfiniteScroll from 'react-infinite-scroller';
+import store from '../../index';
+import * as actions from '../../redux/actions/filterResultsActions';
 import { CarModel } from '../../redux/models/filterResultsModel';
 import CarAd from './CarAd/CarAd';
 import './style.less';
@@ -11,54 +13,56 @@ export interface Props {
 }
 
 export interface State {
-  index: number;
+  allAdsAreLoaded: boolean;
+  items: any;
 }
 
 class FilterResults extends React.Component<Props, State> {
   constructor() {
     super();
     this.state = {
-      index: 20
+      allAdsAreLoaded: false,
+      items: <div className="choose-filters-message">Please choose filter options</div>
     };
   }
 
+  public populateAds = () => {
+    const carAds: any[] = [];
+    this.props.filterResults.forEach((value: CarModel, index: number) => {
+      carAds.push(
+        <Grid item className="ad" key={index}>
+          <CarAd
+            model={value.model}
+            mark={value.mark}
+            description={value.description}
+            price={value.price}
+            year={value.year}
+            images={value.images}
+            mileFrom={value.mileFrom}
+          />
+        </Grid>
+      );
+    });
+    this.setState({
+      items: carAds
+    });
+  };
+
   public loadFunc = () => {
-    if (this.state.index) {
-      this.setState({
-        index: this.state.index + 20
-      });
-    }
+    // call action here
+    this.populateAds();
   };
 
   public render() {
-    const { filterResults } = this.props;
-    const items: any[] = [];
-    filterResults.forEach((value: CarModel, index: number) => {
-      if (index < this.state.index) {
-        items.push(
-          <Grid item className="ad" key={index}>
-            <CarAd
-              model={value.model}
-              mark={value.mark}
-              description={value.description}
-              price={value.price}
-              year={value.year}
-              images={value.images}
-              mileFrom={value.mileFrom}
-            />
-          </Grid>
-        );
-      }
-    });
     return (
       <Grid container className="ads-container">
         <InfiniteScroll
           className="infinite-scroll-container"
           loadMore={this.loadFunc}
-          hasMore={true}
-          loader={<CircularProgress size={50} />}
+          hasMore={this.props.filterResults.length > 0 && !this.state.allAdsAreLoaded}
+          loader={this.state.items.length > 1 ? <CircularProgress size={50} /> : <div />}
         >
-          {items}
+          {this.state.items}
         </InfiniteScroll>
       </Grid>
     );
