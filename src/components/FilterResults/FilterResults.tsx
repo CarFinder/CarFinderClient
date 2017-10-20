c5cc43903c122092bb7c363e0d9d199c84905d93
Elizaveta <lizaburaya@gmail.com>
2017-10-20 14:01:18 +0300

    Replace infinite scroll with load more

--- src/components/FilterResults/FilterResults.tsx
+++ src/components/FilterResults/FilterResults.tsx
@@ -6,6 +6,7 @@ import InfiniteScroll from 'react-infinite-scroller';
 import * as actions from '../../redux/actions/filterResultsActions';
 import { CarModel } from '../../redux/models/filterResultsModel';
 import CarAd from './CarAd/CarAd';
+import Button from 'material-ui/Button';
 import './style.less';
 
 export interface Props {
@@ -34,61 +35,53 @@ class FilterResults extends React.Component<Props, State> {
     };
   }
 
-  public componentWillReceiveProps() {
-    this.populateAds();
-  }
-
-  public populateAds = () => {
-    if (this.props.filterResults.length % 20 !== 0) {
-      this.props.handleSetAdsAsLoaded(true);
+  public componentWillReceiveProps(nextProps: Props) {
+    if (this.props.filterResults) {
+      console.log(this.props.filterResults);
+      const carAds: any[] = [];
+      this.props.filterResults.forEach((value: CarModel, index: number) => {
+        carAds.push(
+          <Grid item className="ad" key={index}>
+            <CarAd
+              model={value.model}
+              mark={value.mark}
+              description={value.description}
+              price={value.price}
+              year={value.year}
+              images={value.images}
+              kms={value.kms}
+            />
+          </Grid>
+        );
+      });
+      this.setState({
+        items: carAds.length ? (
+          carAds
+        ) : (
+          <Typography type="display1">No ads to display, try another filter</Typography>
+        ),
+        skipped: this.state.skipped + carAds.length
+      });
     }
-    const carAds: any[] = [];
-    this.props.filterResults.forEach((value: CarModel) => {
-      carAds.push(
-        <Grid item className="ad" key={value._id}>
-          <CarAd
-            model={value.model}
-            mark={value.mark}
-            description={value.description}
-            price={value.price}
-            year={value.year}
-            images={value.images}
-            kms={value.kms}
-          />
-        </Grid>
-      );
-    });
-    this.setState({
-      items: carAds.length ? (
-        carAds
-      ) : (
-        <Typography type="display1">No ads to display, try another filter</Typography>
-      ),
-      skipped: this.state.skipped + carAds.length
-    });
-  };
-
-  public loadFunc = () => {
-    this.props.handleUpdateAds(
-      this.props.carFilters.currentFilter,
-      this.props.carFilters.sortingParams
-    );
-    this.populateAds();
-  };
+  }
 
   public render() {
     return (
       <Grid container className="ads-container">
         {this.props.loading && <CircularProgress size={50} />}
         {!this.props.loading && (
-          <InfiniteScroll
-            className="infinite-scroll-container"
-            loadMore={this.loadFunc}
-            hasMore={this.props.filterResults.length > 0 && !this.props.adsAreLoaded}
-            loader={this.state.items.length > 1 ? <CircularProgress size={50} /> : <div />}
-          >
+          <div>
             {this.state.items}
-          </InfiniteScroll>
+            <Button
+              onClick={() =>
+                this.props.handleUpdateAds(
+                  this.props.carFilters.currentFilter,
+                  this.props.carFilters.sortingParams
+                )}
+            >
+              Load More
+            </Button>
+          </div>
         )}
       </Grid>
     );
