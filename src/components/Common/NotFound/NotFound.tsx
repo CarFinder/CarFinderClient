011c92cbdce08e08cd5cb436e3df50b2522e73da
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-21 13:17:08 +0300

    NotFound component markup

--- src/components/Common/NotFound/NotFound.tsx
+++ src/components/Common/NotFound/NotFound.tsx
@@ -1,36 +1,24 @@
-import Button from 'material-ui/Button';
-import Card, { CardActions, CardContent } from 'material-ui/Card';
-import Grid from 'material-ui/Grid';
-import Typography from 'material-ui/Typography';
 import * as React from 'react';
 import { Link } from 'react-router-dom';
+
 import './style.less';
 
 const NotFound = () => (
-  <div className="not-found">
-    <Grid container>
-      <Grid item xs={12}>
-        <Grid container align="center" direction="column" justify="center">
-          <Card className="not-found-content">
-            <CardContent>
-              <Typography className="not-found-title" type="headline" component="h3">
-                404 not found
-              </Typography>
-              <Typography type="body1" component="p" color="secondary">
-                Sorry. We couldn't find that page.
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Link to="/">
-                <Button color="primary" dense>
-                  Go to Home
-                </Button>
-              </Link>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-    </Grid>
+  <div className="section">
+    <div className="container">
+      <div className="columns">
+        <div className="column is-centered">
+          <article className="message is-danger">
+            <div className="message-header">
+              <p>404 Not Found</p>
+            </div>
+            <div className="message-body">
+              Sorry. We couldn't find that page.
+            </div>
+          </article>
+        </div>
+      </div>
+    </div>
   </div>
 );
 
