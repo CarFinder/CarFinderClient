a023ef22d52f10554ed5b1cf8e6106319cb4d48f
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-10-23 11:33:25 +0300

    Make price font bigger

--- src/components/FilterResults/CarAd/CarAd.tsx
+++ src/components/FilterResults/CarAd/CarAd.tsx
@@ -29,7 +29,8 @@ const CarAd = (props: Props) => {
       <figure className="media-right">
         <div className="media-info">
           <span className="has-text-warning">
-            <i className="fa fa-usd" aria-hidden="true" />&nbsp;${price}
+            <i className="fa fa-usd" aria-hidden="true" />&nbsp;{' '}
+            <strong className="ad-price">{price}</strong>
           </span>
           <span className="has-text-grey">
             <i className="fa fa-clock-o" aria-hidden="true" />&nbsp;{year}
