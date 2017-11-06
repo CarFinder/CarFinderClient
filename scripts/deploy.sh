b01f97d680b513c6f1be13111b3fde525680b7e2
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-11-06 18:46:09 +0300

    Modify auto-deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -15,10 +15,10 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 #using token clone gh-pages branch
-git clone --quiet --branch=gh-pages https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git gh-pages > /dev/null
+git clone --quiet --branch=master https://${GH_TOKEN}@github.com/${GH_USER}/${GH_REPO}.git master > /dev/null
 
 #go into directory and copy data we're interested in to that directory
-cd gh-pages
+cd master
 cp -Rf $HOME/dist/* .
 
 echo "Allow files with underscore https://help.github.com/articles/files-that-start-with-an-underscore-are-missing/" > .nojekyll
@@ -27,7 +27,7 @@ echo "[View live](https://${GH_USER}.github.io/${GH_REPO}/)" > README.md
 #add, commit and push files
 git add -f .
 git commit -m "Travis build $TRAVIS_BUILD_NUMBER"
-git push -fq origin gh-pages > /dev/null
+git push -fq origin master > /dev/null
 
 echo "Done updating gh-pages\n"
 
