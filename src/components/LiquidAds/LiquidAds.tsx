96f8d5b1489e2b977ddd97c73974b4b08eaafadb
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-11-08 14:51:30 +0300

    Liquid Models component

--- src/components/LiquidAds/LiquidAds.tsx
+++ src/components/LiquidAds/LiquidAds.tsx
@@ -1,11 +1,52 @@
 import * as React from 'react';
 import * as interfaces from '../../interfaces';
+import SliderWithText from '../Common/Slider/SliderWithText';
 
 export interface Props {
   handleGetLiquidAds: () => void;
   liquidAds: interfaces.LiquidAds[];
 }
 
-const LiquidAds = (props: Props) => <div>LiquidAds</div>;
+const LiquidAds = (props: Props) => (
+  <section className="section">
+    <div className="container is-fluid">
+      <div className="columns">
+        <div className="column is-centered">
+          <SliderWithText content={mockData} />
+        </div>
+      </div>
+    </div>
+  </section>
+);
 
 export default LiquidAds;
+
+const mockData: interfaces.LiquidAds[] = [
+  {
+    _id: '1',
+    mark: 'Audi',
+    model: 'A6',
+    median: 50,
+    images: [
+      'https://content.onliner.by/automarket/1589576/800x800/da5f57e0ea91524b6ba08008f6c2004f.jpeg'
+    ]
+  },
+  {
+    _id: '2',
+    mark: 'BMW',
+    model: 'X6',
+    median: 100,
+    images: [
+      'https://content.onliner.by/automarket/1983797/800x800/23e49b984457cde728e2b55245083607.jpeg'
+    ]
+  },
+  {
+    _id: '3',
+    mark: 'Ford',
+    model: 'Mondeo',
+    median: 40,
+    images: [
+      'https://content.onliner.by/automarket/1589576/800x800/a9a889e10cf7245e2c414955fc653382.jpeg'
+    ]
+  }
+];
