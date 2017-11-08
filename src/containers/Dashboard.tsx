26d4a3b85ad894bf4147abf048483d314d7c25bd
Elizaveta <lizaburaya@gmail.com>
2017-11-08 15:26:47 +0300

    Add parameters to state and dispatch

--- src/containers/Dashboard.tsx
+++ src/containers/Dashboard.tsx
@@ -1,13 +1,19 @@
 import { connect, Dispatch } from 'react-redux';
 import UserDashboard from '../components/UserDashboard/UserDashboard';
 import * as interfaces from '../interfaces';
-import * as savedSearchActions from '../redux/actions/savedSearchActions';
+import * as calculateLiquidityActions from '../redux/actions/calculateLiquidityActions';
+import * as carFiltersActions from '../redux/actions/carFiltersActions';
+import * as formStateActions from '../redux/actions/formStateActions';
 import * as savedSearchResultsActions from '../redux/actions/savedSearchResultsActions';
 
 interface DispatchFromProps {
   handleFetchSavedSearchResults: () => void;
   handleRemoveAllFilters: () => void;
   handleRemoveFilterById: (id: string) => void;
+  handleCalculateLiquidity: (data: any) => void;
+  handleFetchMarksValues: () => void;
+  handleFetchBodyTypesValues: () => void;
+  handleFetchModelsValues: (mark: string) => void;
 }
 
 interface StateToProps {
@@ -15,13 +21,17 @@ interface StateToProps {
   loading: boolean;
   language: string;
   savedSearchResults: interfaces.SavedFilterResults[];
+  liquidity: number;
+  filterValues: interfaces.FilterValues;
 }
 
 const mapStateToProps = (state: any) => ({
   loading: state.formState.loading,
   searchError: state.formState.searchError,
   language: state.user.interfaceLanguage,
-  savedSearchResults: state.savedSearchResults
+  savedSearchResults: state.savedSearchResults,
+  filterValues: state.carFilters.filterValues,
+  liquidity: state.calculateLiquidity.result
 });
 
 const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
@@ -29,7 +39,13 @@ const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
   handleFetchSavedSearchResults: () =>
     dispatch(savedSearchResultsActions.fetchSavedSearchResults()),
   handleRemoveFilterById: (id: string) =>
-    dispatch(savedSearchResultsActions.removeSavedFilterById(id))
+    dispatch(savedSearchResultsActions.removeSavedFilterById(id)),
+  handleClearError: () => dispatch(formStateActions.setSearchError('')),
+  handleFetchMarksValues: () => dispatch(carFiltersActions.fetchMarksValues()),
+  handleFetchBodyTypesValues: () => dispatch(carFiltersActions.fetchBodyTypesValues()),
+  handleFetchModelsValues: (mark: string) => dispatch(carFiltersActions.fetchModelsValues(mark)),
+  handleCalculateLiquidity: (data: interfaces.LiquidityFilter) =>
+    dispatch(calculateLiquidityActions.сalculateLiquidity(data))
 });
 
 export default connect<StateToProps, DispatchFromProps>(mapStateToProps, mapDispatchToProps)(
