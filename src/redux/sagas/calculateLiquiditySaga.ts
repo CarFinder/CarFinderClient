c7502f89bfc922832a139bd59eb2bf49582c44aa
Elizaveta <lizaburaya@gmail.com>
2017-11-08 15:35:48 +0300

    Create CalculateLiquidity saga

---
+++ src/redux/sagas/calculateLiquiditySaga.ts
@@ -0,0 +1,24 @@
+import { SagaIterator } from 'redux-saga';
+import { call, put, takeEvery } from 'redux-saga/effects';
+import api from '../../api/api';
+import { transformDataForLiquidity } from '../../utils/utils';
+import * as actionTypes from '../actions/actionTypes';
+import { Action } from './index';
+
+function* callCalculateLiquidity(action: Action) {
+  yield put({ type: actionTypes.SET_LOADING, payload: true });
+  yield put({ type: actionTypes.SET_SEARCH_ERROR, payload: '' });
+  try {
+    const data = transformDataForLiquidity(action.payload);
+    const response = yield call(api.liquidity.getLiquidity, data);
+    yield put({ type: actionTypes.GET_LIQUIDITY, payload: response.data });
+    yield put({ type: actionTypes.SET_LOADING, payload: false });
+  } catch (e) {
+    yield put({ type: actionTypes.SET_SEARCH_ERROR, payload: 'Server-side error' });
+    yield put({ type: actionTypes.SET_LOADING, payload: false });
+  }
+}
+
+export default function* watchCalculateLiquidity(): SagaIterator {
+  yield takeEvery(actionTypes.CALCULATE_LIQUIDITY, callCalculateLiquidity);
+}
