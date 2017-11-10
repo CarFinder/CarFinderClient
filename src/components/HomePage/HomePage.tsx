2400915f86e9652b549617c1bf450a9718173db3
Elizaveta <lizaburaya@gmail.com>
2017-11-10 21:51:19 +0300

    Feature: create landing page

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,10 +1,17 @@
 import * as React from 'react';
+import Contact from './HomePageBlocks/Contact';
+import Features from './HomePageBlocks/Features';
+import Header from './HomePageBlocks/Header';
+import Technologies from './HomePageBlocks/Technologies';
 
 class HomePage extends React.PureComponent<any, any> {
   public render() {
     return (
       <div>
-        <p>Home Page</p>
+        <Header />
+        <Features />
+        <Technologies />
+        <Contact />
       </div>
     );
   }
