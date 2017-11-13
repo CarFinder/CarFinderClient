730d6123b1083a7c7aa05768ed327e03c72c2420
Elizaveta <lizaburaya@gmail.com>
2017-11-13 21:53:00 +0300

    Implement animation in Features block

--- src/components/HomePage/HomePageBlocks/Features.tsx
+++ src/components/HomePage/HomePageBlocks/Features.tsx
@@ -1,48 +1,90 @@
+import classNames from 'classnames';
 import * as React from 'react';
+import Waypoint from 'react-waypoint';
+import './animate.less';
 import './style.less';
 
-const Features = (props: any) => {
-  return (
-    <section className="section section-features">
-      <div className="container">
-        <p className="title">Features</p>
-        <div className="columns">
-          <div className="column section-column">
-            <span className="icon section-icon is-large">
-              <span className="fa fa-3x">
-                <i className="fa fa fa-car" />
-              </span>
-            </span>
-            <p className="title is-4">Search for the best offers from top platforms</p>
-          </div>
-          <div className="column section-column">
-            <span className="icon section-icon is-large">
-              <span className="fa fa-3x">
-                <i className="fa fa-floppy-o" />
-              </span>
-            </span>
-            <p className="title is-4">Save your search filters</p>
-          </div>
-          <div className="column section-column">
-            <span className="icon section-icon is-large">
-              <span className="fa fa-3x">
-                <i className="fa fa-envelope-open-o" />
-              </span>
-            </span>
-            <p className="title is-4">Be the first to get new offers</p>
-          </div>
-          <div className="column section-column">
-            <span className="icon section-icon is-large">
-              <span className="fa fa-3x">
-                <i className="fa fa-line-chart" />
-              </span>
-            </span>
-            <p className="title is-4">Find out how fast you can sell your car</p>
-          </div>
+class Features extends React.PureComponent<any, any> {
+  constructor() {
+    super();
+    this.state = {
+      visibleIcon: false
+    };
+  }
+
+  public render() {
+    return (
+      <section className="section section-features">
+        <div className="container">
+          <p className="title">Features</p>
+          <Waypoint
+            onEnter={() =>
+              this.setState({
+                visibleIcon: true
+              })
+            }
+            onLeave={() =>
+              this.setState({
+                visibleIcon: false
+              })
+            }
+          >
+            <div className="columns">
+              <div className="column section-column">
+                <span
+                  className={classNames('icon section-icon is-large', {
+                    'visible-1': this.state.visibleIcon
+                  })}
+                >
+                  <span className="fa fa-3x">
+                    <i className="fa fa fa-car" />
+                  </span>
+                </span>
+                <p className="title is-4">Search for the best offers from top platforms</p>
+              </div>
+              <div className="column section-column">
+                <span
+                  className={classNames('icon section-icon is-large', {
+                    'visible-2': this.state.visibleIcon
+                  })}
+                >
+                  <span className="fa fa-3x">
+                    <i className="fa fa-floppy-o" />
+                  </span>
+                </span>
+                <p className="title is-4">Save your search filters</p>
+              </div>
+              <div className="column section-column">
+                <span
+                  className={classNames('icon section-icon is-large', {
+                    'visible-3': this.state.visibleIcon
+                  })}
+                >
+                  <span className="fa fa-3x">
+                    <i className="fa fa-envelope-open-o" />
+                  </span>
+                </span>
+                <p className="title is-4">Be the first to get new offers</p>
+              </div>
+              <div className="column section-column">
+                <span
+                  className={classNames('icon section-icon is-large', {
+                    'visible-4': this.state.visibleIcon
+                  })}
+                >
+                  <span className="fa fa-3x">
+                    <i className="fa fa-line-chart" />
+                  </span>
+                </span>
+
+                <p className="title is-4">Find out how fast you can sell your car</p>
+              </div>
+            </div>
+          </Waypoint>
         </div>
-      </div>
-    </section>
-  );
-};
+      </section>
+    );
+  }
+}
 
 export default Features;
