1f6827b1375e9225183f6dc7135ec2ef7e28f16a
Elizaveta <lizaburaya@gmail.com>
2017-11-14 19:15:45 +0300

    Add logic and props to child components

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,17 +1,48 @@
 import * as React from 'react';
+import * as interfaces from '../../interfaces';
 import Contact from './HomePageBlocks/Contact';
 import Features from './HomePageBlocks/Features';
 import Header from './HomePageBlocks/Header';
 import Technologies from './HomePageBlocks/Technologies';
 
-class HomePage extends React.PureComponent<any, any> {
+export interface Props {
+  handleSubmitMessage: (message: any) => void;
+  handleClearAuthError: () => void;
+  handleClearSearchError: () => void;
+  handleGetStats: () => void;
+  loading: boolean;
+  searchError?: any;
+  authError?: any;
+  language: string;
+  successMessage: string;
+  stats: interfaces.ServiceStats;
+}
+
+class HomePage extends React.PureComponent<Props, any> {
+  public handleSubmit = (message: any) => {
+    this.props.handleSubmitMessage(message);
+  };
+
+  public componentDidMount() {
+    this.props.handleClearAuthError();
+    this.props.handleClearSearchError();
+    this.props.handleGetStats();
+  }
+
   public render() {
+    const { language, searchError, authError, loading, successMessage, stats } = this.props;
     return (
       <div>
-        <Header />
-        <Features />
-        <Technologies />
-        <Contact />
+        <Header language={language} searchError={searchError} stats={stats} />
+        <Features language={language} />
+        <Technologies language={language} />
+        <Contact
+          handleSubmit={this.handleSubmit}
+          language={language}
+          loading={loading}
+          successMessage={successMessage}
+          authError={authError}
+        />
       </div>
     );
   }
