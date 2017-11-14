4a1fc0034e038f58dbd7cbb3b085524cc39e2663
Elizaveta <lizaburaya@gmail.com>
2017-11-14 19:20:46 +0300

    Add dispatch and state to Home container

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,26 +1,40 @@
 import { connect, Dispatch } from 'react-redux';
 import HomePage from '../components/HomePage/HomePage';
+import * as interfaces from '../interfaces';
+import * as formStateActions from '../redux/actions/formStateActions';
+import * as getStatsActions from '../redux/actions/getStatsActions';
 import * as submitMessageActions from '../redux/actions/messageActions';
 
 interface DispatchFromProps {
+  handleClearAuthError: () => void;
+  handleClearSearchError: () => void;
+  handleGetStats: () => void;
   handleSubmitMessage: (message: any) => void;
 }
 
 interface StateToProps {
   loading: boolean;
   searchError?: any;
+  authError?: any;
   language: string;
+  successMessage: string;
+  stats: interfaces.ServiceStats;
 }
 
 const mapStateToProps = (state: any) => ({
   loading: state.formState.loading,
   searchError: state.formState.searchError,
+  authError: state.formState.authError,
   language: state.user.interfaceLanguage,
-  successMessage: state.formState.successMessage
+  successMessage: state.formState.successMessage,
+  stats: state.getStats
 });
 
-const mapDispatchToProps = (dispatch: Dispatch<submitMessageActions.SubmitMessage>) => ({
-  handleSubmitMessage: (message: any) => dispatch(submitMessageActions.submitMessage(message))
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({
+  handleSubmitMessage: (message: any) => dispatch(submitMessageActions.submitMessage(message)),
+  handleGetStats: () => dispatch(getStatsActions.getStats()),
+  handleClearAuthError: () => dispatch(formStateActions.setAuthError('')),
+  handleClearSearchError: () => dispatch(formStateActions.setSearchError(''))
 });
 
 export default connect<StateToProps, DispatchFromProps>(mapStateToProps, mapDispatchToProps)(
