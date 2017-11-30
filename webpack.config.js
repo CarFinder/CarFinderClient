5a8b71fe0bdf40f794c344ad9b4ebc387996d5e6
Anatoly Belobrovik <anatoliybelobrovik@gmail.com>
2017-11-30 13:49:51 +0300

    Compress and minify bundle

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,26 @@
 const webpack = require('webpack');
 const path = require('path');
+const CompressionPlugin = require('compression-webpack-plugin');
 const WebpackNotifierPlugin = require('webpack-notifier');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const StyleExtHtmlWebpackPlugin = require('style-ext-html-webpack-plugin');
 
 module.exports = env => {
   return {
-    devtool: 'source-map',
+    devtool: env.NODE_ENV === 'production' ? 'cheap-module-source-map' : 'source-map',
     entry: ['index.tsx'],
     output: {
       filename: 'bundle.js',
       publicPath: 'dist',
       path: path.resolve('./dist')
     },
-  devServer: {
-    port: 3001,
-    proxy: {
-      '/api': 'http://localhost:2999',
-      '/redirect': 'http://localhost:9090'
-    },
+    devServer: {
+      port: 3001,
+      proxy: {
+        '/api': 'http://localhost:2999',
+        '/redirect': 'http://localhost:9090'
+      },
       historyApiFallback: true,
       inline: true,
       stats: {
@@ -74,12 +76,42 @@ module.exports = env => {
           NODE_ENV: JSON.stringify(env.NODE_ENV)
         }
       }),
+      new webpack.optimize.ModuleConcatenationPlugin(),
+      new webpack.HashedModuleIdsPlugin(),
+      new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          warnings: false,
+          screw_ie8: true,
+          conditionals: true,
+          unused: true,
+          comparisons: true,
+          sequences: true,
+          dead_code: true,
+          evaluate: true,
+          if_return: true,
+          join_vars: true
+        },
+        output: {
+          comments: false
+        },
+        sourceMap: true
+      }),
       new OptimizeCssAssetsPlugin({
-      assetNameRegExp: /\.optimize\.css$/g,
-      cssProcessor: require('cssnano'),
-      cssProcessorOptions: { discardComments: { removeAll: true } },
-      canPrint: true
-    })
+        assetNameRegExp: /\.optimize\.css$/g,
+        cssProcessor: require('cssnano'),
+        cssProcessorOptions: { discardComments: { removeAll: true } },
+        canPrint: true
+      }),
+      new StyleExtHtmlWebpackPlugin({
+        minify: true
+      }),
+      new CompressionPlugin({
+        asset: '[path].gz[query]',
+        algorithm: 'gzip',
+        test: /\.js$|\.ts$|\.tsx$|\.css$|\.html$|\.eot?.+$|\.ttf?.+$|\.woff?.+$|\.svg?.+$/,
+        threshold: 10240,
+        minRatio: 0.8
+      })
     ]
   };
 };
